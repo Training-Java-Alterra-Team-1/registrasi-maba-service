597262fc97485ee78b147832a9f85ca6a21d80f8
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-08-16 17:29:38 +0700

    add default data degrees on application turn on

--- src/main/java/com/mahasiswabaru/register/RegisterApplication.java
+++ src/main/java/com/mahasiswabaru/register/RegisterApplication.java
@@ -1,5 +1,7 @@
 package com.mahasiswabaru.register;
 
+import com.mahasiswabaru.register.dto.DegreesDto;
+import com.mahasiswabaru.register.services.DegreesService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +16,12 @@ public class RegisterApplication {
 		SpringApplication.run(RegisterApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner run(A)
+	@Bean
+	public CommandLineRunner run(DegreesService degreesService){
+		return args ->{
+			degreesService.addDegree(new DegreesDto("S1"));
+			degreesService.addDegree(new DegreesDto("S2"));
+			degreesService.addDegree(new DegreesDto("S3"));
+		};
+	}
 }
