f0ceb2cf7ef2155432b0a89d46b945cd9f3d6e03
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-08-16 16:15:32 +0700

    update degree service: get all degrees

--- src/main/java/com/mahasiswabaru/register/RegisterApplication.java
+++ src/main/java/com/mahasiswabaru/register/RegisterApplication.java
@@ -1,7 +1,9 @@
 package com.mahasiswabaru.register;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
@@ -12,4 +14,6 @@ public class RegisterApplication {
 		SpringApplication.run(RegisterApplication.class, args);
 	}
 
+//	@Bean
+//	public CommandLineRunner run(A)
 }
