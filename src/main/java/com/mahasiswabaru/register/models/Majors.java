6b91b13f110a511ee49ccdadbdd1045d02e7f316
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-08-18 17:02:38 +0700

    delete unused import

--- src/main/java/com/mahasiswabaru/register/models/Majors.java
+++ src/main/java/com/mahasiswabaru/register/models/Majors.java
@@ -4,12 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 @Entity(name="Majors")
 @Table(name="majors")
@@ -28,11 +25,9 @@ public class Majors extends Audit<String> implements Serializable {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id", nullable = false, insertable = true, referencedColumnName = "id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Departments departments;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "degree_id", nullable = false, insertable = true, referencedColumnName = "id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Degrees degrees;
 }
