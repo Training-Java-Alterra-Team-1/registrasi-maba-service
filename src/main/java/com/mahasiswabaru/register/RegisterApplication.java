2180bbb5349f08e905ec4503f82748bf9150ad20
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-08-18 20:27:26 +0700

    add remarks on inserting initial data to database

--- src/main/java/com/mahasiswabaru/register/RegisterApplication.java
+++ src/main/java/com/mahasiswabaru/register/RegisterApplication.java
@@ -20,53 +20,53 @@ public class RegisterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner run(DegreesService degreesService, DepartmentsService departmentsService, MajorsService majorsService){
-		return args ->{
-			degreesService.addDegree(new DegreesDto("S1"));
-			degreesService.addDegree(new DegreesDto("S2"));
-			degreesService.addDegree(new DegreesDto("S3"));
-
-			departmentsService.addDepartment(new DepartmentDto("Mathematics and Natural Science"));
-			departmentsService.addDepartment(new DepartmentDto("Electrical Engineering"));
-			departmentsService.addDepartment(new DepartmentDto("Industrial Technology"));
-
-			majorsService.addMajor(new MajorsDto("Mathematics", 1, 1));
-			majorsService.addMajor(new MajorsDto("Mathematics", 1, 2));
-			majorsService.addMajor(new MajorsDto("Mathematics", 1, 3));
-			majorsService.addMajor(new MajorsDto("Statistics", 1, 1));
-			majorsService.addMajor(new MajorsDto("Statistics", 1, 2));
-			majorsService.addMajor(new MajorsDto("Statistics", 1, 3));
-			majorsService.addMajor(new MajorsDto("Chemistry", 1,1));
-			majorsService.addMajor(new MajorsDto("Chemistry", 1,2));
-			majorsService.addMajor(new MajorsDto("Chemistry", 1,3));
-			majorsService.addMajor(new MajorsDto("Biology", 1,1));
-			majorsService.addMajor(new MajorsDto("Biology", 1,2));
-			majorsService.addMajor(new MajorsDto("Biology", 1,3));
-			majorsService.addMajor(new MajorsDto("Physics", 1,1));
-			majorsService.addMajor(new MajorsDto("Physics", 1,2));
-			majorsService.addMajor(new MajorsDto("Physics", 1,3));
-			majorsService.addMajor(new MajorsDto("Astronomics", 1,1));
-			majorsService.addMajor(new MajorsDto("Astronomics", 1,2));
-			majorsService.addMajor(new MajorsDto("Astronomics", 1,3));
-			majorsService.addMajor(new MajorsDto("Computer Science", 1,1));
-			majorsService.addMajor(new MajorsDto("Computer Science", 1,2));
-			majorsService.addMajor(new MajorsDto("Computer Science", 1,3));
-
-			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,1));
-			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,2));
-			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,3));
-			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,1));
-			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,2));
-			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,3));
-			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,1));
-			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,2));
-			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,3));
-
-			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 1));
-			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 2));
-			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 3));
-		};
-	}
+//	unmark these comments below to add initial data to the database
+//	@Bean
+//	public CommandLineRunner run(DegreesService degreesService, DepartmentsService departmentsService, MajorsService majorsService){
+//		return args ->{
+//			degreesService.addDegree(new DegreesDto("S1"));
+//			degreesService.addDegree(new DegreesDto("S2"));
+//			degreesService.addDegree(new DegreesDto("S3"));
+//
+//			departmentsService.addDepartment(new DepartmentDto("Mathematics and Natural Science"));
+//			departmentsService.addDepartment(new DepartmentDto("Electrical Engineering"));
+//			departmentsService.addDepartment(new DepartmentDto("Industrial Technology"));
+//
+//			majorsService.addMajor(new MajorsDto("Mathematics", 1, 1));
+//			majorsService.addMajor(new MajorsDto("Mathematics", 1, 2));
+//			majorsService.addMajor(new MajorsDto("Mathematics", 1, 3));
+//			majorsService.addMajor(new MajorsDto("Statistics", 1, 1));
+//			majorsService.addMajor(new MajorsDto("Statistics", 1, 2));
+//			majorsService.addMajor(new MajorsDto("Statistics", 1, 3));
+//			majorsService.addMajor(new MajorsDto("Chemistry", 1,1));
+//			majorsService.addMajor(new MajorsDto("Chemistry", 1,2));
+//			majorsService.addMajor(new MajorsDto("Chemistry", 1,3));
+//			majorsService.addMajor(new MajorsDto("Biology", 1,1));
+//			majorsService.addMajor(new MajorsDto("Biology", 1,2));
+//			majorsService.addMajor(new MajorsDto("Biology", 1,3));
+//			majorsService.addMajor(new MajorsDto("Physics", 1,1));
+//			majorsService.addMajor(new MajorsDto("Physics", 1,2));
+//			majorsService.addMajor(new MajorsDto("Physics", 1,3));
+//			majorsService.addMajor(new MajorsDto("Astronomics", 1,1));
+//			majorsService.addMajor(new MajorsDto("Astronomics", 1,2));
+//			majorsService.addMajor(new MajorsDto("Astronomics", 1,3));
+//			majorsService.addMajor(new MajorsDto("Computer Science", 1,1));
+//			majorsService.addMajor(new MajorsDto("Computer Science", 1,2));
+//			majorsService.addMajor(new MajorsDto("Computer Science", 1,3));
+//
+//			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,1));
+//			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,2));
+//			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,3));
+//			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,1));
+//			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,2));
+//			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,3));
+//			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,1));
+//			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,2));
+//			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,3));
+//
+//			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 1));
+//			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 2));
+//			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 3));
+//		};
+//	}
 }
