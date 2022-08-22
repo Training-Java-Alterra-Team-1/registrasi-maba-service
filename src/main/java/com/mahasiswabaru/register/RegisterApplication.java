6b4e74743e0b871e5a17d266c04574349d30f4b2
jojiejo <jodyramadhan@gmail.com>
2022-08-22 20:55:11 +0700

    Comment out commandliner

--- src/main/java/com/mahasiswabaru/register/RegisterApplication.java
+++ src/main/java/com/mahasiswabaru/register/RegisterApplication.java
@@ -28,49 +28,49 @@ public class RegisterApplication {
 	@Bean
 	public CommandLineRunner run(DegreesService degreesService, DepartmentsService departmentsService, MajorsService majorsService){
 		return args ->{
-			degreesService.addDegree(new DegreesDto("S1"));
-			degreesService.addDegree(new DegreesDto("S2"));
-			degreesService.addDegree(new DegreesDto("S3"));
-
-			departmentsService.addDepartment(new DepartmentDto("Mathematics and Natural Science"));
-			departmentsService.addDepartment(new DepartmentDto("Electrical Engineering"));
-			departmentsService.addDepartment(new DepartmentDto("Industrial Technology"));
-
-			majorsService.addMajor(new MajorsDto("Mathematics", 1, 1));
-			majorsService.addMajor(new MajorsDto("Mathematics", 1, 2));
-			majorsService.addMajor(new MajorsDto("Mathematics", 1, 3));
-			majorsService.addMajor(new MajorsDto("Statistics", 1, 1));
-			majorsService.addMajor(new MajorsDto("Statistics", 1, 2));
-			majorsService.addMajor(new MajorsDto("Statistics", 1, 3));
-			majorsService.addMajor(new MajorsDto("Chemistry", 1,1));
-			majorsService.addMajor(new MajorsDto("Chemistry", 1,2));
-			majorsService.addMajor(new MajorsDto("Chemistry", 1,3));
-			majorsService.addMajor(new MajorsDto("Biology", 1,1));
-			majorsService.addMajor(new MajorsDto("Biology", 1,2));
-			majorsService.addMajor(new MajorsDto("Biology", 1,3));
-			majorsService.addMajor(new MajorsDto("Physics", 1,1));
-			majorsService.addMajor(new MajorsDto("Physics", 1,2));
-			majorsService.addMajor(new MajorsDto("Physics", 1,3));
-			majorsService.addMajor(new MajorsDto("Astronomics", 1,1));
-			majorsService.addMajor(new MajorsDto("Astronomics", 1,2));
-			majorsService.addMajor(new MajorsDto("Astronomics", 1,3));
-			majorsService.addMajor(new MajorsDto("Computer Science", 1,1));
-			majorsService.addMajor(new MajorsDto("Computer Science", 1,2));
-			majorsService.addMajor(new MajorsDto("Computer Science", 1,3));
-
-			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,1));
-			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,2));
-			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,3));
-			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,1));
-			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,2));
-			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,3));
-			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,1));
-			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,2));
-			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,3));
-
-			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 1));
-			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 2));
-			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 3));
+//			degreesService.addDegree(new DegreesDto("S1"));
+//			degreesService.addDegree(new DegreesDto("S2"));
+//			degreesService.addDegree(new DegreesDto("S3"));
+//
+//			departmentsService.addDepartment(new DepartmentDto("Mathematics and Natural Science"));
+//			departmentsService.addDepartment(new DepartmentDto("Electrical Engineering"));
+//			departmentsService.addDepartment(new DepartmentDto("Industrial Technology"));
+//
+//			majorsService.addMajor(new MajorsDto("Mathematics", 1, 1));
+//			majorsService.addMajor(new MajorsDto("Mathematics", 1, 2));
+//			majorsService.addMajor(new MajorsDto("Mathematics", 1, 3));
+//			majorsService.addMajor(new MajorsDto("Statistics", 1, 1));
+//			majorsService.addMajor(new MajorsDto("Statistics", 1, 2));
+//			majorsService.addMajor(new MajorsDto("Statistics", 1, 3));
+//			majorsService.addMajor(new MajorsDto("Chemistry", 1,1));
+//			majorsService.addMajor(new MajorsDto("Chemistry", 1,2));
+//			majorsService.addMajor(new MajorsDto("Chemistry", 1,3));
+//			majorsService.addMajor(new MajorsDto("Biology", 1,1));
+//			majorsService.addMajor(new MajorsDto("Biology", 1,2));
+//			majorsService.addMajor(new MajorsDto("Biology", 1,3));
+//			majorsService.addMajor(new MajorsDto("Physics", 1,1));
+//			majorsService.addMajor(new MajorsDto("Physics", 1,2));
+//			majorsService.addMajor(new MajorsDto("Physics", 1,3));
+//			majorsService.addMajor(new MajorsDto("Astronomics", 1,1));
+//			majorsService.addMajor(new MajorsDto("Astronomics", 1,2));
+//			majorsService.addMajor(new MajorsDto("Astronomics", 1,3));
+//			majorsService.addMajor(new MajorsDto("Computer Science", 1,1));
+//			majorsService.addMajor(new MajorsDto("Computer Science", 1,2));
+//			majorsService.addMajor(new MajorsDto("Computer Science", 1,3));
+//
+//			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,1));
+//			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,2));
+//			majorsService.addMajor(new MajorsDto("Electrical Engineering", 2,3));
+//			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,1));
+//			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,2));
+//			majorsService.addMajor(new MajorsDto("Automation System Engineering", 2,3));
+//			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,1));
+//			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,2));
+//			majorsService.addMajor(new MajorsDto("Power System Engineering", 2,3));
+//
+//			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 1));
+//			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 2));
+//			majorsService.addMajor(new MajorsDto("Industrial Engineering", 1, 3));
 		};
 	}
 }
