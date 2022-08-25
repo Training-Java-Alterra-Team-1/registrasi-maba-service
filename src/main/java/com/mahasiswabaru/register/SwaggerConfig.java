83b7ca8b73eefcf563b0c6616168070b3ab9cf03
Jalaluddin <jalaluddin.almursyidy@ikonsultan.com>
2022-08-25 15:32:54 +0700

    Add authorization to SwaggerConfig

--- src/main/java/com/mahasiswabaru/register/SwaggerConfig.java
+++ src/main/java/com/mahasiswabaru/register/SwaggerConfig.java
@@ -4,15 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private ApiKey apiKey(){
+        return new ApiKey(AUTHORIZATION_HEADER, "JWT", "header");
+    }
+
+    List<SecurityReference> defaultAuth(){
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("ROLE_ADMIN", "roleAdministrator");
+        authorizationScopes[1] = new AuthorizationScope("ROLE_LECTURER", "roleLecturer");
+        authorizationScopes[2] = new AuthorizationScope("ROLE_STUDENT", "roleStudent");
+
+        return Arrays.asList(new SecurityReference(AUTHORIZATION_HEADER, authorizationScopes));
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
